ffc4f5cccc73a50788516df2816b7d4f2fff9db6
NidaElvinMertoglu <128838910+NidaElvinMertoglu@users.noreply.github.com>
2023-06-08 20:54:12 +0300

    Insan class nesneleri

--- Main.py
+++ Main.py
@@ -4,3 +4,7 @@ from Calisan import Calisan
 from MaviYaka import MaviYaka
 from BeyazYaka import BeyazYaka
 import pandas as pd
+
+insan1 = Insan("12345678900", "Nira", "Saat", 30, "Kadın", "Turk")
+insan2 = Insan("98765432109", "Alexander", "Hamilton", 25, "Erkek", "Amerikali")
+print(str(insan1))
